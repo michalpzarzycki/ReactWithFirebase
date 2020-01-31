d2c2693e4971b41990fd76434bc9dcc006cc9220
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-01-31 14:27:42 +0100

    Developes useValidationForm hook

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
-
+import useFormValidation from './useFormValidation'
+const INITIAL_STATE = {
+    name: "",
+    email: "",
+    password: ""
+}
 const Login = () => {
-const [login, setLogin] = useState(true);
-    return(
+    const { handleChange, handleSubmit, values } = useFormValidation(INITIAL_STATE);
+    const [login, setLogin] = useState(true);
+
+    return (
         <div>
-<h2>{login ? "Login" : "Create Account"}</h2>
-<form>
-    {!login && <input type="text" placeholder="Your name" autoComplete="off"/>}
-    <input type="email" placeholder="Your email" autoComplete="off"/>
-    <input type="password" placeholder="Your password" />
-    <div>
-        <button type="submit">SUBMIT</button>
-        <button type="button" onClick={()=>setLogin(prevState=>!prevState)}>
-    {login ? "need to create an account" : "already have an account?"}
-        </button>
-    </div>
-</form>
+            <h2>{login ? "Login" : "Create Account"}</h2>
+            <form>
+                {!login && <input name="name" type="text" placeholder="Your name" value={values.name} onChange={handleChange} autoComplete="off" />}
+                <input name="email" type="email" placeholder="Your email" value={values.email} onChange={handleChange} autoComplete="off" />
+                <input name="password" type="password" placeholder="Your password" value={values.password} onChange={handleChange} />
+                <div>
+                    <button type="submit" onClick={handleSubmit}>SUBMIT</button>
+                    <button type="button" onClick={() => setLogin(prevState => !prevState)}>
+                        {login ? "need to create an account" : "already have an account?"}
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
---
+++ src/components/Auth/useFormValidation.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+
+const useFormValidation = (initialState) => {
+
+    const [values, setValues] = useState(initialState)
+
+    const handleChange = (event) => {
+        event.persist();
+        setValues(previousValues => ({
+            ...previousValues,
+            [event.target.name]: event.target.value
+        }))
+    }
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log({ values })
+    }
+ return { handleChange, handleSubmit, values }
+}
+
+export default useFormValidation;
