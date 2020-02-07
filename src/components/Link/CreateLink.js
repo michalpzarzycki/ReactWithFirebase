bb134d8303bd459c4e66e769c5895b63f1eef0d1
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-02-07 15:18:58 +0100

    BUG FIX (syntax errors and changed user.id to user.uid)

--- src/components/Link/CreateLink.js
+++ src/components/Link/CreateLink.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import useFormValidation from '../Auth/useFormValidation';
 import validateCreate from '../Auth/validateCreate'
+import FirebaseContext from '../../firebase/context';
 const INITIAL_STATE = {
     description:"",
     url:""
 }
-const CreateLink = () => {
+const CreateLink = (props) => {
+    const { firebase, user }  = React.useContext(FirebaseContext)
 const { handleSubmit, handleChange, values, errors } = useFormValidation(INITIAL_STATE, validateCreate, handleCreateLink)
-
+console.log("USER:", user)
 function handleCreateLink() {
-    console.log('link created')
+if(!user) {
+    props.history.push('/login')
+} else {
+    const { url, description } = values;
+    const newLink = {
+        url, 
+        description,
+        postedBy: {
+            id: user.uid,
+            name: user.displayName
+        },
+        votes: [],
+        comments: [],
+        created: Date.now()
+    }
+    firebase.db.collection('links').add(newLink);
+    props.history.push('/')
+}
+
 }
     return(
         <form onSubmit={handleSubmit}>
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 
 import firebaseConfig from './config'
 
@@ -9,6 +10,7 @@ import firebaseConfig from './config'
       constructor() {
           app.initializeApp(firebaseConfig);
           this.auth = app.auth();
+          this.db = app.firestore();
       }
      async register(name, email, password) {
      const newUser = await this.auth.createUserWithEmailAndPassword(
